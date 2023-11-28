8ccbda4b63848d01aa5a854205ef35a9a4e48f11
ariff06 <ariffauzy74@gmail.com>
2023-11-28 10:54:15 +0700

    add animation

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,3 @@
-// toggle icon navbar
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-};
-
-// handle menu link click
-document.querySelectorAll('.navbar a').forEach(link => {
-    link.addEventListener('click', () => {
-        // close the menu when a link is clicked
-        menuIcon.classList.remove('bx-x');
-        navbar.classList.remove('active');
-    });
-});
-
 // scroll section
 let sections = document.querySelectorAll('section');
 let navlinks = document.querySelectorAll('.navbar a');
@@ -40,6 +22,12 @@ window.onscroll = () => {
 
             // Add 'active' class to the current section link
             document.querySelector('header nav a[href="#' + id + '"]').classList.add('active');
+
+            // Add 'show-animate' class to the current section
+            sec.classList.add('show-animate');
+        } else {
+            // Remove 'show-animate' class from sections that are not in view
+            sec.classList.remove('show-animate');
         }
     });
 };
